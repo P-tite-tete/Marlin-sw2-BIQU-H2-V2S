25e2d6f84bdde5637b676ceb77dd31e1ee5b7a58
P'tite tête <petitcuenotc@icloud.com>
2022-11-10 13:26:42 +0100

    Callage BLTouch et Home

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1375,7 +1375,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -32, -30, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -33.5, -32.5, -2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1578,11 +1578,11 @@
 #define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -2
-#define Y_MIN_POS -5
+#define X_MIN_POS -10
+#define Y_MIN_POS 2
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS (X_BED_SIZE+5)
+#define Y_MAX_POS (Y_BED_SIZE+20)
 #define Z_MAX_POS 400
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
