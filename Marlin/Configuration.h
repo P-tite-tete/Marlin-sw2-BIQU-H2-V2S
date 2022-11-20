f06142f06ea7e1e969f662475d86205ceec8c170
P'tite tête <petitcuenotc@icloud.com>
2022-11-20 18:07:03 +0100

    Modification dim plateau

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1574,15 +1574,15 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 300
-#define Y_BED_SIZE 300
+#define X_BED_SIZE 305
+#define Y_BED_SIZE 305
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -10
-#define Y_MIN_POS 2
+#define X_MIN_POS -5
+#define Y_MIN_POS -3
 #define Z_MIN_POS 0
 #define X_MAX_POS (X_BED_SIZE+5)
-#define Y_MAX_POS (Y_BED_SIZE+20)
+#define Y_MAX_POS (Y_BED_SIZE+15)
 #define Z_MAX_POS 400
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
